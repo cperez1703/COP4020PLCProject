bc3e027b25a29ce71048a509e1223cb00421b083
Sonny-Cao <116669832+Sonny-Cao@users.noreply.github.com>
2023-10-01 15:37:42 -0400

    Starter files for HW1 added

--- src/edu/ufl/cise/cop4020fa23/Token.java
+++ src/edu/ufl/cise/cop4020fa23/Token.java
@@ -1,18 +1,18 @@
 /*Copyright 2023 by Beverly A Sanders
- * 
- * This code is provided for solely for use of students in COP4020 Programming Language Concepts at the 
- * University of Florida during the fall semester 2023 as part of the course project.  
- * 
- * No other use is authorized. 
- * 
- * This code may not be posted on a public web site either during or after the course.  
+ *
+ * This code is provided for solely for use of students in COP4020 Programming Language Concepts at the
+ * University of Florida during the fall semester 2023 as part of the course project.
+ *
+ * No other use is authorized.
+ *
+ * This code may not be posted on a public web site either during or after the course.
  */
 package edu.ufl.cise.cop4020fa23;
 
 import java.util.Objects;
 
 /**
- * 
+ *
  */
 public class Token implements IToken {
 
@@ -57,8 +57,8 @@ public class Token implements IToken {
 	}
 
 
-	
-	@Override 
+
+	@Override
 	public String toString() {
 		return ("["+kind+" "+text()+"]" );
 	}
